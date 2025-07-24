ce7b0c5b7bc98ea0b3224743bdd91de422d35c20
Ali Wangara <Aliwangara63@gmail.com>
2025-07-24 16:32:02 +0300

    added more functions like clear interval

--- script.js
+++ script.js
@@ -1,65 +1,68 @@
-
 const watchDisplay = document.querySelector('.watch-display');
 const startBtn = document.getElementById('start-btn');
 const pauseBtn = document.getElementById('pause-btn');
 const resetBtn = document.getElementById('reset-btn');
 
 
+
 let seconds = 0;
-let minute = 0;
+let minutes = 0;
 let hours = 0;
 let timer;
-let isRunning = false;
+let isRunning;
 
 function display(){
     seconds++;
 
     if(seconds === 60){
         seconds = 0;
-        minute++;
+        minutes++
     }
-    if(minute === 60){
-        minute = 0;
+    if(minutes === 60){
+        minutes =0;
         hours++
     }
-    
-    const sec = seconds <10 ? '0'+seconds:seconds;
-    const min = minute<10 ? '0' + minute : minute;
-    const hrs = hours<10 ? '0' + hours:hours
 
-    
-    watchDisplay.textContent = `${hrs} : ${min} : ${sec}`
+    const sec = seconds < 10 ? '0' + seconds:seconds;
+    const min = minutes < 10 ? '0' + minutes:minutes;
+    const hrs = hours < 10 ? '0' + hours:hours;
 
+    watchDisplay.textContent = ` ${hrs} : ${min} : ${sec}`;
 
 }
-
 
 startBtn.addEventListener('click', ()=>{
     if(!isRunning){
         timer = setInterval(display, 1000)
-        isRunning = true
+        isRunning = true;
+
     }
+
+
 
 
 })
 
 pauseBtn.addEventListener('click', ()=>{
     if(isRunning){
-        clearInterval(timer)
-        isRunning=false
+        clearInterval(timer);
+        isRunning = false;
     }
-
 })
 
 resetBtn.addEventListener('click', ()=>{
     if(isRunning){
         clearInterval(timer);
         isRunning = false;
-        hours = 0;
-        minute = 0;
-        seconds = 0;
+         hours = 0;
+         minutes = 0;
+         seconds = 0;
+
     }
 
     watchDisplay.textContent = `00 : 00 : 00`
 
+
 })
+
+
